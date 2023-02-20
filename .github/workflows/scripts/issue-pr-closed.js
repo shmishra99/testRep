8b24d95db40c0c9b2df752ad823cde74a6fdfb74
Shivam Mishra <shivammishr@google.com>
2023-02-20 16:31:41 +0530

    message

--- .github/workflows/scripts/issue-pr-closed.js
+++ .github/workflows/scripts/issue-pr-closed.js
@@ -4,28 +4,38 @@ module.exports = async ({github,context}) => {
     // console.log('issue author is ', context.payload.issue.user.login);
     // console.log('issue latest commenter is ', context.payload.sender.login);
     
-    let issuess = await github.rest.issues.listForRepo({
+    let issuessList = await github.rest.issues.listForRepo({
          owner: context.repo.owner,
          repo: context.repo.repo,
          state:'closed',
          labels:'stalled'
 
     })
-
-    // for(let i=0;i<issuess.length;i++){
-         
-    //     // //obj call for stale label remove 
-    //     // let staleRM = 
-
-    //     // await rmLabel()
-            
-
-    // }
-
+  
+    let issues = issuessList.data;
     
+    
+    for(let i=0;i<issues.length;i++){
+         
+       let issue = issues[i]
 
-    console.log("line 3",JSON.stringify(issuess.data[0].labels) )
+       let queryObj = {}
+       queryObj["issue_number"] = issue.number;
+       queryObj['owner'] = context.repo.owner;
+       queryObj["repo"] = context.repo.repo;
+       
+       //call to remove stale
+       queryObj["name"] = 'stalled'
+       console.log("line 30",queryObj)
+       await rmLabel(queryObj)
+      
+       //call to remove a stat:awaiting response
 
+       queryObj["name"] = "stat:awaiting response"
+       console.log("line 30",queryObj)
+       await rmLabel(queryObj)
+
+    }
 
 
     console.log("issue trigger")
@@ -34,12 +44,6 @@ module.exports = async ({github,context}) => {
 
 async function rmLabel(queryObj){
   
-    await github.rest.issues.removeLabel({
-        issue_number: context.issue.number,
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        name: "stale"
-
-    })
+    await github.rest.issues.removeLabel(queryObj)
 
 }
