3d9f26dd08b3b77f906d56f4d857671124d41725
Shivam Mishra <shivammishr@google.com>
2023-02-20 14:08:17 +0530

    message

--- .github/workflows/scripts/issue-pr-closed.js
+++ .github/workflows/scripts/issue-pr-closed.js
@@ -1,6 +1,12 @@
 module.exports = async ({github,context}) => {  
 
-    console.log("line 3", github.job)
+    let issuess = await github.rest.issues.listForRepo({
+          
+        owner: context.repo.owner,
+        repo: context.repo.repo
+
+    })
+    console.log("line 3",issuess )
     console.log("issue trigger")
 
 }
