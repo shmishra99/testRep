90413d6ab809c38acb21ba35ddc79793143b6ffc
Shivam Mishra <shivammishr@google.com>
2023-02-20 14:00:43 +0530

    message

--- .github/workflows/scripts/author-comment.js
+++ .github/workflows/scripts/author-comment.js
@@ -1,41 +1,32 @@
-module.exports = async ({github,context}) => {  
-        
+module.exports = async ({ github, context }) => {
     console.log('entered unmark for issueid = ', context.payload.issue.number);
     console.log('issue author is ', context.payload.issue.user.login);
     console.log('issue latest commenter is ', context.payload.sender.login);
-
     if (context.payload.issue.state !== 'closed' &&
-      context.payload.issue.user.login == context.payload.sender.login) {
-      let labelRM = []
-      for (const label of context.payload.issue.labels) {
-      
-        if (label.name.includes("stale")) {
-          console.log("Removing label: stale")
-          await github.rest.issues.removeLabel({
-            issue_number: context.issue.number,
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            name:"stale"
-        
-          })
-         
+        context.payload.issue.user.login == context.payload.sender.login) {
+        let labelRM = []
+        for (const label of context.payload.issue.labels) {
+
+            if (label.name.includes("stale")) {
+                console.log("Removing label: stale")
+                await github.rest.issues.removeLabel({
+                    issue_number: context.issue.number,
+                    owner: context.repo.owner,
+                    repo: context.repo.repo,
+                    name: "stale"
+
+                })
+            }
+            if (label.name.includes("stat:awaiting response")) {
+                console.log("Removing label : stat:awaiting response")
+                await github.rest.issues.removeLabel({
+                    issue_number: context.issue.number,
+                    owner: context.repo.owner,
+                    repo: context.repo.repo,
+                    name: "stat:awaiting response"
+
+                })
+            }
         }
-
-        if (label.name.includes("stat:awaiting response")) {         
-          console.log("Removing label : stat:awaiting response")
-          await github.rest.issues.removeLabel({
-            issue_number: context.issue.number,
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            name:"stat:awaiting response"
-        
-          })
-        }
-
-     
-      }       
-
     }
-
-
 }
--- .github/workflows/scripts/issue-pr-closed.js
+++ .github/workflows/scripts/issue-pr-closed.js
@@ -1,7 +1,6 @@
 module.exports = async ({github,context}) => {  
-        
-    console.log("line 3", github)
+
+    console.log("line 3", github.job)
     console.log("issue trigger")
-       
 
 }
