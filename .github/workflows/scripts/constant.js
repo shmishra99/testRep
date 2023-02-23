1c752221e13ca53f94c79174e612aefda98ca020
Shivam Mishra <shivammishr@google.com>
2023-02-23 13:40:25 +0530

    message

--- .github/workflows/scripts/constant.js
+++ .github/workflows/scripts/constant.js
@@ -20,7 +20,8 @@ let CONSTENT_VALUES = {
             NO:'No',
             CSAT_INCLUDES_REPO:  'tensorflow,tfx,transform,hub,data-validation,model-analysis,' +
             'serving,tfjs,models,mediapipe,keras,TestGitRepository,testRep',
-            BASE_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScOLT8zeBHummIZFnfr9wqvxYzWD1DAypyvNia5WVIWtFANYg/viewform?',
+            BASE_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSfaP12TRhd9xSxjXZjcZFNXPG' +
+        'k4kc1-qMdv3gc6bEP90vY1ew/viewform?',
             MEDIA_PIPE_BASE_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScOLT8zeBHummIZFnfr9wqvxYzWD1DAypyvNia5WVIWtFANYg/viewform?',
             SATISFACTION_PARAM: 'entry.85265664=',
             ISSUEID_PRAM: '&entry.2137816233=',
