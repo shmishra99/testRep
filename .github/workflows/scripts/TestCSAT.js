88068a28b7b15a39f7a9df38cf65f2672da3da28
Shivam Mishra <shivammishr@google.com>
2023-04-01 21:25:15 +0530

    comment

--- .github/workflows/scripts/TestCSAT.js
+++ .github/workflows/scripts/TestCSAT.js
@@ -1,5 +1,14 @@
 module.exports = async ({ github, context }) => {
 
-  console.log("line 3",github)
-  console.log("kube 4" ,context)
+//   console.log("line 3",github)
+//   console.log("kube 4" ,context)
+
+  let logs = await github.rest.actions.downloadJobLogsForWorkflowRun({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    job_id: 4583967721
+})
+  console.log("logss ",logs)
+
 }
