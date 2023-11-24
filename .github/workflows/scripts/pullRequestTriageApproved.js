f766addb158c09ba8b2bea0585947d7393a28990
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-11-24 15:57:01 +0530

    Update pullRequestTriageApproved.js

--- .github/workflows/scripts/pullRequestTriageApproved.js
+++ .github/workflows/scripts/pullRequestTriageApproved.js
@@ -3,7 +3,7 @@ const CONSTENT_VALUES = require('./constant')
 
 /*
 Invoked from pullRequestTriageApproved.yaml file to add label 
-for "ready to pull" and "kokoro:force-run" when PR review submitted. 
+for "ready to pull" when PR review submitted as approved. 
 */
 
 
@@ -12,7 +12,7 @@ module.exports = async ({ github, context }) => {
         !context.payload.pull_request.base.ref.includes('main')) { //change master to main
         return false;
     }
-    let labelsToAdd = CONSTENT_VALUES.MODULE.pullRequestTriageApprovedLabels;
+   let labelsToAdd = ['ready to pull']
    if((context.payload.review.state === 'approved')){ 
         github.rest.issues.addLabels({
         owner: context.repo.owner,
